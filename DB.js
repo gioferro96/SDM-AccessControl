c858ae89c09875c896920fed7d237de6d913e134
gioferro96 <gioferro96@gmail.com>
2019-11-21 11:00:09 +0100

    database code

---
+++ DB.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+//instantiate express
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+let port = 5000;
+let dbConnected = false;
+
+let mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "patient"
+});
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+	dbConnected = true;
+});
+
+
+app.route('/data')
+	.get((req,res)=>{
+		let id = req.body.id;
+		let type = req.body.type;
+
+		if(dbConnected){
+			if((id != null)&&(type != null)){
+				let types = type.split(",");
+				let sql = "select data from data where userid="+id+" AND type='"+types[0]+"'";
+				for (i = 1; i < types.length; i++){
+					sql += " OR type='"+types[i]+"'";
+				}
+				console.log(sql);
+				con.query(sql, function (err, result) {
+					if (err) throw err;
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'plain/text');
+					res.send(result);
+				});
+			}else if (id != null){
+				let sql = "select data from data where userid="+id;
+				con.query(sql, function (err, result) {
+					if (err) throw err;
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'plain/text');
+					res.send(result);
+				});
+			}else{
+				res.statusCode = 400;
+				setHeader('Content-Type', 'plain/text');
+				res.send("Error: missing information in the request (id, type)");
+			}
+			
+		}else{
+			res.statusCode = 500;
+			setHeader('Content-Type', 'plain/text');
+			res.send("Error: no db connection");
+		}
+
+
+
+	})
+	.post((req,res)=>{
+		let upid = req.body.uploaderid;
+		let uname = req.body.username;
+		let type = req.body.type;
+		//check type is unique
+		let data = req.body.data;
+		let sql = "select id from users where name='"+uname+"'";
+		con.query(sql, function (err, result) {
+			if (err) throw err;
+			let id = result[0].id;
+			var dt = new Date();
+			let today = dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate();
+			let sql2 = "insert into data values ("+id+", "+upid+", '"+today+"', '"+type+"', '"+data+"')";
+			con.query(sql2, function (err, result) {
+				if(err) throw err;
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'plain/text');
+				res.send("ok");
+			});
+		});
+
+	});
+
+
+app.listen(port);
+
+//check status
+console.log('Server running at http://localhost:' + port);
