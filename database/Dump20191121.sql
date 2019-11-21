6f8a4e5391080a95204202e75f9aa5e635bc9690
gioferro96 <gioferro96@gmail.com>
2019-11-21 12:41:51 +0100

    updated DB dump with correct tables

--- database/Dump20191121.sql
+++ database/Dump20191121.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `patient` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `patient`;
 -- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
 --
 -- Host: localhost    Database: patient
@@ -24,14 +26,11 @@ DROP TABLE IF EXISTS `data`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `data` (
   `userid` int(11) NOT NULL,
-  `uploaderid` int(11) NOT NULL,
   `uploadDate` date DEFAULT NULL,
   `type` varchar(20) DEFAULT NULL,
-  `data` varchar(400) DEFAULT NULL,
+  `info` varchar(400) DEFAULT NULL,
   KEY `userid` (`userid`),
-  KEY `uploaderid` (`uploaderid`),
-  CONSTRAINT `data_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`),
-  CONSTRAINT `data_ibfk_2` FOREIGN KEY (`uploaderid`) REFERENCES `users` (`id`)
+  CONSTRAINT `data_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -41,8 +40,34 @@ CREATE TABLE `data` (
 
 LOCK TABLES `data` WRITE;
 /*!40000 ALTER TABLE `data` DISABLE KEYS */;
-INSERT INTO `data` VALUES (12,14,'2013-09-23','ECG','good ecg results, no problem'),(12,14,'2019-11-21','check','astmha (o come cazzo si scrive)'),(10,20,'2019-11-21','check','feels happy'),(11,2,'2019-11-21','check','feels tired, better to sleep a lot tomorrow');
+INSERT INTO `data` VALUES (12,'2013-09-18','check','everything ok'),(11,'2019-11-21','check','feels tired, better to sleep a lot tomorrow');
 /*!40000 ALTER TABLE `data` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tempData`
+--
+
+DROP TABLE IF EXISTS `tempData`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tempData` (
+  `userid` int(11) NOT NULL,
+  `uploaderName` varchar(40) NOT NULL,
+  `uploadDate` date DEFAULT NULL,
+  `type` varchar(20) DEFAULT NULL,
+  `info` varchar(400) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tempData`
+--
+
+LOCK TABLES `tempData` WRITE;
+/*!40000 ALTER TABLE `tempData` DISABLE KEYS */;
+INSERT INTO `tempData` VALUES (11,'hospital1','2019-11-21','check','feels tired, better to sleep a lot tomorrow'),(12,'hospital1','2019-11-21','check','is very hungry');
+/*!40000 ALTER TABLE `tempData` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -82,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-21 11:06:02
+-- Dump completed on 2019-11-21 12:41:03
