a9fc9f8fd5e2893811221e67fcc2fdf0572b2a76
gioferro96 <gioferro96@gmail.com>
2019-11-26 11:21:28 +0100

    changed delete in tempData

--- database/dumpStructure.sql
+++ database/dumpStructure.sql
@@ -34,6 +34,15 @@ CREATE TABLE `data` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `data`
+--
+
+LOCK TABLES `data` WRITE;
+/*!40000 ALTER TABLE `data` DISABLE KEYS */;
+INSERT INTO `data` VALUES (12,'2013-09-18','check','everything ok'),(11,'2019-11-21','check','feels tired, better to sleep a lot tomorrow'),(12,'2019-11-21','check','feels tired, better to sleep a lot tomorrow'),(12,'2019-11-21','bloodAnalysis','too high colesterolo'),(10,'2019-11-21','bloodAnalysis','too high colesterolo'),(11,'2019-11-21','doctor','feeling sore'),(12,'2019-11-21','doctor','feeling sore');
+/*!40000 ALTER TABLE `data` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tempData`
@@ -47,10 +56,21 @@ CREATE TABLE `tempData` (
   `uploaderName` varchar(40) NOT NULL,
   `uploadDate` date DEFAULT NULL,
   `type` varchar(20) DEFAULT NULL,
-  `info` varchar(2048) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `info` varchar(400) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `tempData`
+--
+
+LOCK TABLES `tempData` WRITE;
+/*!40000 ALTER TABLE `tempData` DISABLE KEYS */;
+INSERT INTO `tempData` VALUES (11,'hospital1','2019-11-21','check','feels tired, better to sleep a lot tomorrow',1),(12,'MSThospital','2019-11-21','ECG','no bad signs in the ecg',2);
+/*!40000 ALTER TABLE `tempData` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -71,6 +91,15 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (0,'Antonio','1983-09-09','Milano','doctor'),(2,'Health Club 1',NULL,'Hengelo','healthclub'),(10,'Riccardo','1990-03-20','Milano','patient'),(11,'Damiano','2000-03-20','Bassano','patient'),(12,'Giovanni','2000-10-20','Vicenza','patient'),(14,'Hospital1',NULL,'Enschede','hospital'),(20,'MSThospital',NULL,'Enschede','hospital'),(21,'The Doctor','1965-12-20','Vicenza','doctor'),(22,'Doctor House','1975-01-24','New York','doctor'),(27,'Memorial Hospital',NULL,'Washington','hospital'),(30,'Public Hospital',NULL,'Amsterdam','hospital'),(31,'Training center',NULL,'Amsterdam','healthclub'),(32,'Run Boy Run',NULL,'Paris','healthclub'),(40,'Employer','1980-12-02','Utrecht','employer'),(41,'The Boss','1960-10-02','Arnhem','employer'),(7408,'Silvio2','1984-09-09','Mezzocorona','employer'),(12622,'Tony','2019-09-09','Home','patient'),(21545,'Silvio','1983-09-09','Trento','employer'),(25414,'Antonio2','1983-09-09','Milano','doctor');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -81,4 +110,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-25 18:50:04
+-- Dump completed on 2019-11-26 11:20:01
